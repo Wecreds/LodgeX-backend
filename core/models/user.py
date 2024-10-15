b5d140e41db2a86c92bc508fd4250bbc7fad1347
Wecreds <wecredsbr@gmail.com>
2024-10-15 11:08:56 -0300

    feat: Criação do modelo usuário(em progresso).

---
+++ core/models/user.py
@@ -0,0 +1,79 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+    Group,
+    Permission
+)
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+class UserManager(BaseUserManager):
+
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address.")
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(
+        max_length=255,
+        unique=True,
+        verbose_name=_("email"),
+        help_text=_("Email")
+        )
+    name = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        verbose_name=_("name"),
+        help_text=_("Username")
+    )
+    telephone = models.IntegerField(
+        unique=True,
+        verbose_name=_("telephone"),
+        help_text=_("Telefone celular do usuário.")
+    )
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name=_("Usuário está ativo"),
+        help_text=_("Indica que este usuário está ativo.")
+    )
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_set',  
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_permission_user_set', 
+        blank=True
+    )
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        """Meta options for the model."""
+
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
