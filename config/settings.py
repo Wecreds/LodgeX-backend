3d24ceed4a6922e6a95353028f1af949f7319b54
Wecreds <wecredsbr@gmail.com>
2024-10-22 10:19:05 -0300

    fix: Trocando "Language_Code" para Ingles.

--- config/settings.py
+++ config/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
-LANGUAGE_CODE = "pt-br"
+LANGUAGE_CODE = "en-us"
 TIME_ZONE = "America/Sao_Paulo"
 USE_I18N = True
 USE_TZ = True
