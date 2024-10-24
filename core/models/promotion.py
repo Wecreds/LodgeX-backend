7e5c26cda350fa6ed583c26e8d0f293a836e8dfb
Wecreds <Wecredsbr@gmail.com>
2024-10-24 12:53:38 -0300

    feat: Fazendo cupoms de desconto e promoções se auto-deletarem ao passar da data de expiração.

--- core/models/promotion.py
+++ core/models/promotion.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
+class PromotionManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(expiration_date__gte=timezone.now().date())
 class Promotion(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False, default="No Promotion Name Given")
     discount_percentage = models.IntegerField()
     expiration_date = models.DateField()
     description = models.CharField(max_length=200)
+
+    objects = PromotionManager()
 
     def __str__(self):
         return f"{self.name} - Expiration: {self.expiration_date} ({self.discount_percentage}% Discount)"
