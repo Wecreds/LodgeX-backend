3af231b2f5e6966ca62e7a29aa75657dc7ba3c14
Wecreds <Wecredsbr@gmail.com>
2024-10-24 13:52:00 -0300

    feat: Fazendo endpoint para devolver apenas os feedbacks aprovados.

--- core/views/feedback.py
+++ core/views/feedback.py
@@ -1,3 +1,6 @@
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Feedback
@@ -6,3 +9,9 @@ from core.serializers import FeedbackSerializer
 class FeedbackViewSet(ModelViewSet):
     queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
+
+    @action(detail=False, methods=["get"])
+    def approved(self, request):
+        queryset = Feedback.objects.filter(review_status=Feedback.FeedbackStatus.APPROVED)
+        serializer = FeedbackSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
