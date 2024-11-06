36d1e3b72e51858d64e5069d0c2416b2fde5f356
Wecreds <Wecredsbr@gmail.com>
2024-11-06 16:25:15 -0300

    fix: removendo erro que fazia com que a senha do usuario fosse bugada ao atualizar atributos.

--- core/serializers/user.py
+++ core/serializers/user.py
@@ -8,14 +8,16 @@ class UserSerializer(ModelSerializer):
         fields = "__all__"
         depth = 1
 
-        def update(self, instance, validated_data):
-            current_user = self.context.get('request').user
+    def update(self, instance, validated_data):
+        current_user = self.context.get('request').user
             
-            if instance != current_user:
-                raise serializers.ValidationError("You can only alter your own user.")
+        if instance != current_user:
+            raise serializers.ValidationError("You can only alter your own user.")
             
-            for attr, value in validated_data.items():
-                setattr(instance, attr, value)
+        validated_data.pop('password', None)
+        
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
             
-            instance.save()
-            return instance
+        instance.save()
+        return instance
